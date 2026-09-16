c61d788895d4f1805d7e352ee07938d18cb5a1be
ofts-cqm <qianmuchen.sam@gmail.com>
2026-09-16 19:17:38 -0400

    feat: initial commit

---
+++ src/client/java/net/ofts/replay_mcp/client/ReplayMCPClient.java
@@ -0,0 +1,10 @@
+package net.ofts.replay_mcp.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReplayMCPClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/ofts/replay_mcp/ReplayMCP.java
@@ -0,0 +1,10 @@
+package net.ofts.replay_mcp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ReplayMCP implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
